5c26c987e627850494c62b17eb18ffe932912178
Aayush Rathore <aayu.r.2003@gmail.com>
2024-01-26 09:18:06 +0530

    Basic Backend setup completed

--- apps/server/environment.d.ts
+++ apps/server/environment.d.ts
@@ -2,6 +2,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MONGO_DB_URL: string;
+      CORS_ORIGIN: string;
+      CLOUD_STORAGE_NAME: string;
+      CLOUD_STORAGE_API_KEY: string;
+      CLOUD_STORAGE_API_SECRET: string;
       NODE_ENV: "development" | "production";
     }
   }
